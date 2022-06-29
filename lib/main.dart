a7db4c319eeea31ac8a72c1fc9981b0d652952aa
Mi Nombre <rodrigosebastianbak@hotmail.com>
2022-06-29 18:25:52 +0200

    app_yoga

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:app_yoga/screens/login/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yoga App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,33 @@
+import 'package:app_yoga/screens/login/components/background_image_clipper.dart';
+import 'package:app_yoga/screens/login/components/circle_button.dart';
+import 'package:app_yoga/screens/login/components/login_credentials.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                BackgroundImage(),
+                LoginCredentials(),
+              ],
+            ),
+            CircleButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
